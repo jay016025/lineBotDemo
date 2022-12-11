f7f5c729c7773e8547f0fff1c670e7b2213f0a87
jay <a0970532713@gmail.com>
2022-12-11 17:05:36 +0800

    commit #6 Create a API query message list of the user from MongoDB

--- src/main/java/idv/jay/lineBotDemo/controller/LineBotMessageController.java
+++ src/main/java/idv/jay/lineBotDemo/controller/LineBotMessageController.java
@@ -14,9 +14,8 @@ import idv.jay.lineBotDemo.controller.request.SendMessageRequest;
 import idv.jay.lineBotDemo.service.MessageServiceFacade;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,6 +30,11 @@ public class LineBotMessageController {
 
     @Autowired
     private MessageServiceFacade messageServiceFacade;
+
+    @GetMapping("message/{userId}")
+    public ResponseEntity<List<idv.jay.lineBotDemo.repository.entity.Message>> getMessagesByUser(@PathVariable String userId) {
+        return ResponseEntity.ok().body(messageServiceFacade.findMessagesByUserId(userId));
+    }
 
     @PostMapping("sendMessage")
     public void sendMessage(@RequestBody SendMessageRequest request) {
