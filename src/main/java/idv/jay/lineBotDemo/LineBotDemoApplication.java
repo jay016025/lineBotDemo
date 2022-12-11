74790d433a944872d173d79a5886e30d1bcacc77
jay <a0970532713@gmail.com>
2022-12-11 16:40:01 +0800

    Commit #1: Set up a Sprint Boot project

---
+++ src/main/java/idv/jay/lineBotDemo/LineBotDemoApplication.java
@@ -0,0 +1,13 @@
+package idv.jay.lineBotDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineBotDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineBotDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idv/jay/lineBotDemo/LineBotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.jay.lineBotDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineBotDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
